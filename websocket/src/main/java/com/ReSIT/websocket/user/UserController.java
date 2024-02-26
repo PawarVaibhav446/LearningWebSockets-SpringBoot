8dadf17feee2da1a21e677a814a5944fcc920545
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-02-26 16:53:05 +0530

    Completed UserController

--- websocket/src/main/java/com/ReSIT/websocket/user/UserController.java
+++ websocket/src/main/java/com/ReSIT/websocket/user/UserController.java
@@ -1,9 +1,45 @@
 package com.ReSIT.websocket.user;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+	private UserService userService;
+
+	public UserController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
+	@MessageMapping("/user.addUser")
+	@SendTo("/user/public")
+	public User addUser(
+			@Payload User user
+	) {
+       userService.saveUser(user);
+       return user;
+    }
+	
+	@MessageMapping("/user.disconnectUser")
+    @SendTo("/user/public")
+    public User disconnectUser(
+            @Payload User user
+    ) {
+        userService.disconnectUser(user);
+        return user;
+    }
+	
+	@GetMapping("/users")
+    public ResponseEntity<List<User>> findConnectedUsers() {
+        return ResponseEntity.ok(userService.findConnectedUser());
+    }
 	
 }
